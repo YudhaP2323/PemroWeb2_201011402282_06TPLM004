7d11c21f078a42dcd2cd25b58102cf8c435f48e1
YudhaP2323 <133026073+YudhaP2323@users.noreply.github.com>
2024-03-20 15:39:03 +0700

    Add files via upload

---
+++ modul_4/lat1.php
@@ -0,0 +1,3 @@
+<?Php
+For ($bil1= 1; $bil1 <= 25; $bil1++) Print ("$bil1	");
+?>
---
+++ modul_4/lat2.php
@@ -0,0 +1,9 @@
+<?php
+echo"tabel perkalian <br>";
+echo"=================<br>";
+$i = 15;
+while ($i <= 45){
+    echo " 12 x $i =". 12 * $i . "<br>";
+ $i+=2;
+}
+?>
